8504a5a55a178c43cce4879a6dd38165c37b086c
Will Hoover <ugate.relay@gmail.com>
2018-10-19 12:25:24 -0400

    Update index.js

--- package/scripts/index.js
+++ package/scripts/index.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function loadVersions(event) {
       if (req.status !== 200) return console.warn(req.status + ' (' + req.statusText + '): Unable to capture ' + sel.dataset.jsonUrl);
       var vrs = JSON.parse(req.responseText);
       var i = vrs.length;
-      while (i-- >= 0) { // add in reverse order
+      while (i--) { // add in reverse order
         opt = document.createElement('option');
         opt.setAttribute('value', vrs[i]);
         opt.appendChild(document.createTextNode(vrs[i]));
