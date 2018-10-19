551161d9e39763a9eba9c9e29c7e8329b9e1b800
ugate <ugate.public@gmail.com>
2018-10-19 12:28:17 -0400

    versions.json now captured from latest/root so current versions will show up for prior doc versions [skip ci]

--- jsdoc/static/package/scripts/index.js
+++ jsdoc/static/package/scripts/index.js
@@ -5,10 +5,11 @@ window.addEventListener('load', function loadVersions(event) {
   req.overrideMimeType('application/json');
   req.open('GET', sel.dataset.jsonUrl, true);
   req.onreadystatechange = function() {
-    if (req.readyState === 4 && req.status === '200') {
+    if (req.readyState === 4) {
+      if (req.status !== 200) return console.warn(req.status + ' (' + req.statusText + '): Unable to capture ' + sel.dataset.jsonUrl);
       var vrs = JSON.parse(req.responseText);
       var i = vrs.length;
-      while (i-- >= 0) { // add in reverse order
+      while (i--) { // add in reverse order
         opt = document.createElement('option');
         opt.setAttribute('value', vrs[i]);
         opt.appendChild(document.createTextNode(vrs[i]));
