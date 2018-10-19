3e7428f4d430363946d030b3003489ea248eb47e
Will Hoover <ugate.relay@gmail.com>
2018-10-19 12:23:20 -0400

    Update index.js

--- package/scripts/index.js
+++ package/scripts/index.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function loadVersions(event) {
   req.open('GET', sel.dataset.jsonUrl, true);
   req.onreadystatechange = function() {
     if (req.readyState === 4) {
-      if (req.status !== '200') return console.warn(req.status + ' (' + req.statusText + '): Unable to capture ' + sel.dataset.jsonUrl);
+      if (req.status !== 200) return console.warn(req.status + ' (' + req.statusText + '): Unable to capture ' + sel.dataset.jsonUrl);
       var vrs = JSON.parse(req.responseText);
       var i = vrs.length;
       while (i-- >= 0) { // add in reverse order
